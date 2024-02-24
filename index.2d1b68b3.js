d9adc341df01e5f3a662364e842e213817a7c129
Artem200921 <134134012+Artem200921@users.noreply.github.com>
2024-02-24 09:50:50 +0000

    Deploying to gh-pages from @ Artem200921/Group_1@a0fa73b869cc683ad4f467cf008a9567b1bcef08 🚀

--- index.2d1b68b3.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".slider"),t=document.querySelectorAll(".slide"),n=t[0].offsetHeight,o=0;setInterval((function(){++o===t.length&&(o=0),e.style.transform="translateY(-".concat(n*o,"px)")}),3e3)}));
-//# sourceMappingURL=index.2d1b68b3.js.map
